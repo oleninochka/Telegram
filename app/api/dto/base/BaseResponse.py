4290b5eb667b1ec5e7b382a30e686a247918fae6
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2023-07-31 10:13:37 +0300

    🐛 Fix timezone

--- app/api/dto/base/BaseResponse.py
+++ app/api/dto/base/BaseResponse.py
@@ -4,12 +4,18 @@ from typing import Dict
 
 import dacite
 from dacite import Config
+from pytz import timezone
 
 from app.api.dto.base.Serializable import Serializable
 
 
 @dataclass
 class BaseResponse(Serializable):
+    @staticmethod
+    def datetime_from_string(s: str) -> datetime:
+        tz = timezone('Europe/Moscow')
+        return datetime.fromisoformat(s).astimezone(tz)
+
     @classmethod
     def parse(cls, data: Dict):
-        return dacite.from_dict(cls, data, Config({datetime: datetime.fromisoformat}))
+        return dacite.from_dict(cls, data, Config({datetime: BaseResponse.datetime_from_string}))
