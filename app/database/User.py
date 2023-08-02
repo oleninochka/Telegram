b41f1ba4eb6c134fb0457185fb08ca37c4ad4630
Krasnopolsky Ivan <i1.krasnopolskiy@gmail.com>
2023-08-02 17:31:31 +0000

    🗃️ Fix user column

--- app/database/User.py
+++ app/database/User.py
@@ -1,12 +1,12 @@
-from peewee import CharField, IntegerField
+from peewee import CharField, BigIntegerField
 
 from app.database.BaseModel import BaseModel
 
 
 class User(BaseModel):
     id = CharField(null=False, index=True)
-    chat_id = IntegerField(null=False, index=True)
-    telegram_id = IntegerField(null=False)
+    chat_id = BigIntegerField(null=False, index=True)
+    telegram_id = BigIntegerField(null=False)
 
     class Meta:
         table_name = "User"
